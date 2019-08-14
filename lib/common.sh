90f750f7ead2670214010a8a35190db14f9515c0
lufei <very90@foxmail.com>
2019-08-14 17:01:07 +0800

    new chk_service_up function

--- lib/common.sh
+++ lib/common.sh
@@ -42,5 +42,24 @@ chk_cmd_exists()
   return $rc
 }
 
+
+chk_service_up()
+{
+  local rc=0
+  for service
+    do systemctl is-active $service || \
+      {
+        echo INFO: $service not active.
+        rc=$((rc+1))
+      }
+    done
+  [ $rc -le 255 ] || \
+    {
+      echo "WARNING: return value is greater than 255"
+      rc=255
+    }
+  return $rc
+}
+
 # get block device size:
 # blockdev --getsize64 /dev/sdc7
